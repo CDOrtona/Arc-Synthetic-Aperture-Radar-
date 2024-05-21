a106fa0b471a1650750d479427eb051150b609cc
CDOrtona <github@cdmails.anonaddy.com>
2024-05-21 21:04:31 +0200

    minor fixes

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clc
 clear
 close all
 
-% FMCW Simulation of a Circular SAR system
+% Simulation of a Circular SAR system based on FMCW chirp
 
 %% Anonymous Functions and constants
 
@@ -28,13 +28,17 @@ ROI_w = 180;
 fc = 79e9;
 lambda = c/fc;
 
-% I am assuming the maximum range is the same as the width
+% I am assuming the maximum range is the same as the length of the ROI
 range_max = ROI_l;
 % the upsweept time depends on the maximum unambiguous range
 % usually it is taken to be either 5 or 6 times greater the nominal value
 % unlike the pulsed chirp, FMCW chirp does not have a PRI time
 % it is not a monostatic system, we are using two antennas 
-tm = 5.5*range2time(range_max);
+tm = 6*range2time(range_max);
+% tm = 68.9*10^-6;
+
+prf = 1e4;
+pri = 1\prf;
 
 
 range_res = 0.043;
@@ -53,6 +57,14 @@ fb_max = range2beat(range_max, sweep_slope);
 fs = max(2.*fb_max,bw);
 fs = round(fs*tm)./tm;
 
+
+
+% %% Pulsed chirp signal
+% 
+% waveform = phased.LinearFMWaveform('SampleRate',fs, ...
+%     'PRF',prf,'OutputFormat','Pulses','NumPulses',1,'SweepBandwidth',bw, ...
+%     'DurationSpecification','Duty cycle','DutyCycle',duty);
+
 %% FMCW chirp signal
 
 waveform = phased.FMCWWaveform('SweepTime',tm,'SweepBandwidth',bw, ...
@@ -67,28 +79,26 @@ title('FMCW signal spectrogram');
 
 %% Radar Scene model 
 
-format long
+radius_platform = 13e-2;
 
-r_platform = 13e-2;
-
-T = ((ROI_w/ang_res)*tm);
-tStep = 0:tm:T;
+T = ((ROI_w/ang_res)*pri);
+tStep = 0:pri:T;
 t = 0:size(tStep, 2)-1;
 vel = (2.*pi)./T;
 
 % waypoints model for a circular trajectory
 % initial positions of the radar platform
-x0 = r_platform;
+x0 = radius_platform;
 y0 = 0;
 z0 = 0;
 
 % angular velocity for a semi-circle 
-angVel = (pi)./T; 
+w = (pi)./T; 
 % trajectory equation of a uniform circular motion
-law = angVel*tStep;  % theta = theta0 + w*t [radians]
+traj = w*tStep;  % theta = theta0 + w*t [radians]
 
-x = r_platform.*cos(law);
-y = r_platform.*sin(law);
+x = radius_platform.*cos(traj);
+y = radius_platform.*sin(traj);
 z = z0*ones(size(t));
 wpts = [tStep.' x.' y.' z.'];
 
@@ -96,7 +106,7 @@ radarPlatform = phased.Platform('MotionModel','Custom','CustomTrajectory',wpts);
 
 % figure;
 % X = [];
-% tstep = tm;
+% tstep = pri;
 % nsteps = 361;
 % for k = 1:nsteps
 %     [pos,vel] = radarPlatform(tstep);
@@ -109,49 +119,34 @@ radarPlatform = phased.Platform('MotionModel','Custom','CustomTrajectory',wpts);
 %% Target Model
 
 num_targets = 3;
-targetPos= ([1,2,0; -2,2,0; 4,3,0])'; 
+targetPos= ([1,2,0;-2,2,0; 4,2,0])'; 
 targetVel = zeros(num_targets);
 target = phased.RadarTarget('OperatingFrequency', fc, 'MeanRCS', db2pow(10)*ones(num_targets, 1)');
 pointTargets = phased.Platform('InitialPosition', targetPos,'Velocity',targetVel);
 
-figure
-plot(targetPos(1,1),targetPos(2,1),'*g')
-hold on
-plot(targetPos(1,2),targetPos(2,2),'*r')
-hold on
-plot(targetPos(1,3),targetPos(2,3),'*b')
-hold on
+% figure
+% plot(targetPos(1,1),targetPos(2,1),'*g')
+% hold on
+% plot(targetPos(1,2),targetPos(2,2),'*r')
+% hold on
+% plot(targetPos(1,3),targetPos(2,3),'*b')
+% hold on
 
-% plot(X(:,1),X(:,2),'o');
-% title('Configurazione SCENA')
-xlim([-6 6])
-ylim([-6 6])
 
 %% Antenna parameters
 
-% MODIFICARE F RANGE
-txantenna =phased.IsotropicAntennaElement('FrequencyRange', [79e9 81e9]);
+txantenna =phased.IsotropicAntennaElement('FrequencyRange', [79e9 83e9]);
 %txantenna = phased.CosineAntennaElement('FrequencyRange', [77e9 81e9]);
 rxantenna = clone(txantenna);
 
 % figure;
 % pattern(txantenna,fc)
 
-ant_aperture = 6.06e-4;                         % in square meter
-ant_gain = aperture2gain(ant_aperture,lambda);  % in dB
-
-tx_ppower = db2pow(5)*1e-3;                     % in watts
-tx_gain = 9+ant_gain;                           % in dB
-
-rx_gain = 15+ant_gain;                          % in dB
-rx_nf = 4.5;                                    % in dB
-
-
 peakpower = 10;
 txgain = 36.0;
 transmitter = phased.Transmitter( ...
-    'PeakPower',tx_ppower, ...
-    'Gain',tx_gain, ...
+    'PeakPower',peakpower, ...
+    'Gain',txgain, ...
     'InUseOutputPort',true);
 radiator = phased.Radiator( ...
     'Sensor',txantenna, ...
@@ -168,17 +163,13 @@ collector = phased.Collector( ...
     'Sensor',rxantenna, ...
     'PropagationSpeed',c, ...
     'OperatingFrequency',fc);
+
 rxgain = 42.0;
 noisefig = 10;
 receiver = phased.ReceiverPreamp( ...
     'SampleRate',fs, ...
-    'Gain',rx_gain, ...
-    'NoiseFigure',rx_nf);
-
-% specanalyzer = dsp.SpectrumAnalyzer('SampleRate',fs,...
-%     'PlotAsTwoSidedSpectrum',true,...
-%     'Title','Spectrum for received and dechirped signal',...
-%     'ShowLegend',true);
+    'Gain',rxgain, ...
+    'NoiseFigure',noisefig);
 
 %% CSAR Simulation 
 
@@ -186,19 +177,11 @@ nSweep = length(t);
 s = complex(zeros(waveform.SampleRate*waveform.SweepTime,nSweep));
 s_unchirped = zeros(size(s));
 
-poss = [];
-
 for m = 1:nSweep
     % Update radar and target positions
-    [radarPos,radarVel] = radarPlatform(tm);
+    [radarPos,radarVel] = radarPlatform(pri);
     
-    poss = cat(1, poss, reshape(radarPos, [1, 3]));
-
-    % THIS NEEDS TO BE FIXED
-    % radarPos = X(m,:)';
-    % radarVel = X_vel(m,:)';
-
-    [targetPos, targetVel] = pointTargets(waveform.SweepTime);
+    [targetPos, targetVel] = pointTargets(pri);
 
     % Get the range and angle to the point targets
     [targetRange, targetAngle] = rangeangle(targetPos, radarPos);
@@ -224,7 +207,6 @@ for m = 1:nSweep
     dechirpedSig = dechirp(rxSig,sig);
     
     % Visualize the spectrum
-    %specanalyzer([rxSig dechirpedSig]);
     s_unchirped(:,m) = rxSig;
     s(:,m) = dechirpedSig;
 end
@@ -257,63 +239,14 @@ fs_adc= fb_max;
 step_cut=round(size(s,1)/round(tm*fs_adc));
 s_sampled=s(1:step_cut:end,:);
 
-
-%% Range and Doppler estimation 
-
-% rngdopresp = phased.RangeDopplerResponse('PropagationSpeed',c,...
-%     'DopplerOutput','Speed','OperatingFrequency',fc,'SampleRate',fs,...
-%     'RangeMethod','FFT','SweepSlope',sweep_slope,...
-%     'RangeFFTLengthSource','Property','RangeFFTLength',2048,...
-%     'DopplerFFTLengthSource','Property','DopplerFFTLength',256);
-% 
-% figure;
-% plotResponse(rngdopresp,s_sampled);                     % Plot range Doppler map
-% axis([-v_max v_max 0 range_max])
-
-%% FFT tests
-
-% s_fft = fft(s_dec, [], 1);
-% s_fft = abs(s_fft);
-% s_fft = s_fft./max(s_fft);
-% % s_fft1 = beat2range(s_fft(:,1), sweep_slope);
-% % s_fft2 = beat2range(s_fft(:,2), sweep_slope);
-% 
-% f = 1:size(s_fft, 1);
-% range_raw=c*f/(2*sweep_slope);
-% range=(ROI_l/range_raw(end))*range_raw';
-% 
-% figure;
-% plot(range, s_fft(:,1), 'g'); 
-% axis([0 5.5 0 1]);
-
-
-%% FFT-RANGE
-% mix_cut = slcimg;
-% Mix_fft_2S=(fft(mix_cut,[],1)); %fft in matrice opera lungo colonne(X,[],1) per farlo lungo righe deve essere(X,[],2)
-% Mix_fft_2S = fftshift((Mix_fft_2S/length(Mix_fft_2S)),1);%fft in matrice opera lungo colonne(X,1) per farlo lungo righe deve essere(X,2
-% lm_2S=size(Mix_fft_2S,1);
-% f_2S= fs_adc/lm_2S *(-lm_2S/2:lm_2S/2-1);
-% figure
-% plot(abs(Mix_fft_2S(:,1)));
-% hold on
-% plot(abs(Mix_fft_2S(:,end)),'r');
-% title('FFT-DS')
-% % 
-% % FFT Singole Side
-% lm=round(lm_2S/2);
-% Mix_fft = 2*Mix_fft_2S(lm-1:-1:1,:);
-% f=(f_2S(round(lm_2S/2)+1:end)); 
-% range_raw=c*f/(2*sweep_slope);
-% range=(maxRange/range_raw(end))*range_raw';
-% 
-% figure
-% plot(range(1:end),abs(Mix_fft(:,1)));
-% hold on
-% plot(range(1:end),abs(Mix_fft(:,end)),'r');
-% % plot(abs(Mix_fft(1:end,:)))
-% title('FFT-SS')
-% xlim([0 6])
-% ylabel('Amplitude')
+%% FFT
+y = abs(fft(s_sampled,2.^nextpow2(size(s_sampled, 1)),1));
+fs_fft = size(y, 1);
+faxis = linspace(-fs_fft/2,fs_fft/2,fs_fft);
+plot(faxis, y(:,1), 'g')
+hold on
+plot(faxis, y(:,end), 'b')
+hold off;
 
 %% Imaging through Back-Projection 1
 
@@ -334,7 +267,7 @@ i_k = 0:fastTime_sample-1; % (i-1)
 
 % tau = 2*R/c -> R is the distance from the radar to the target 
 % R is a row 1xM, where M is the number of slow-time samples
-tau = @(r_l, phi_l, phi_m) sqrt(r_l.^2+r_platform.^2-2.*r_platform.*r_l*cosd(phi_m-phi_l)).*(2./c);
+tau = @(r_l, phi_l, phi_m) sqrt(r_l.^2+radius_platform.^2-2.*radius_platform.*r_l*cosd(phi_m-phi_l)).*(2./c);
 a = @(r_l, phi_l, phi_m, i_k) exp(1i.*2.*pi.*(fc.*tau(r_l, phi_l, phi_m) - (sweep_slope./2)*tau(r_l, phi_l, phi_m).^2 + i_k'*(bw./fastTime_sample)*tau(r_l, phi_l, phi_m)));
 
 % image matrix ixl
@@ -351,6 +284,7 @@ toc
 
 figure;
 imagesc(abs(g));
+
 
 %% from Polar to Cartesian
 
@@ -381,3 +315,56 @@ xlabel('X (meters)');
 ylabel('Y (meters)');
 title('Interpolated Intensity in Cartesian Coordinates');
 colorbar;
+
+
+
+%% --------------------- TESTS -------------------------
+
+%% Range and Doppler estimation 
+
+% rngdopresp = phased.RangeDopplerResponse('PropagationSpeed',c,...
+%     'DopplerOutput','Speed','OperatingFrequency',fc,'SampleRate',fs,...
+%     'RangeMethod','FFT','SweepSlope',sweep_slope,...
+%     'RangeFFTLengthSource','Property','RangeFFTLength',2048,...
+%     'DopplerFFTLengthSource','Property','DopplerFFTLength',256);
+% 
+% figure;
+% plotResponse(rngdopresp,s_sampled);                     % Plot range Doppler map
+% axis([-v_max v_max 0 range_max])
+
+%% FFT
+y = abs(fft(s_sampled,2.^nextpow2(size(s_sampled, 1)),1));
+fs_fft = size(y, 1);
+faxis = linspace(-fs_fft/2,fs_fft/2-1,fs_fft);
+plot(faxis, y(:,1), 'g')
+hold on
+plot(faxis, y(:,end), 'b')
+hold off;
+
+%% FFT-RANGE
+% mix_cut = s_dec;
+% Mix_fft_2S=(fft(mix_cut,[],1)); %fft in matrice opera lungo colonne(X,[],1) per farlo lungo righe deve essere(X,[],2)
+% Mix_fft_2S = fftshift((Mix_fft_2S/length(Mix_fft_2S)),1);%fft in matrice opera lungo colonne(X,1) per farlo lungo righe deve essere(X,2
+% lm_2S=size(Mix_fft_2S,1);
+% f_2S= fs_adc/lm_2S *(-lm_2S/2:lm_2S/2-1);
+% figure
+% plot(abs(Mix_fft_2S(:,1)));
+% hold on
+% plot(abs(Mix_fft_2S(:,end)),'r');
+% title('FFT-DS')
+% % 
+% % FFT Singole Side
+% lm=round(lm_2S/2);
+% Mix_fft = 2*Mix_fft_2S(lm-1:-1:1,:);
+% f=(f_2S(round(lm_2S/2)+1:end)); 
+% range_raw=c*f/(2*sweep_slope);
+% range=(maxRange/range_raw(end))*range_raw';
+
+% figure
+% plot(range(1:end),abs(Mix_fft(:,1)));
+% hold on
+% plot(range(1:end),abs(Mix_fft(:,end)),'r');
+% % plot(abs(Mix_fft(1:end,:)))
+% title('FFT-SS')
+% xlim([0 6])
+% ylabel('Amplitude')
